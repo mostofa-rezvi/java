ae9701ec8344c7b57c583dd8c17ebf9c171fc68d
mostofa-rezvi <mostofa.aminur.rezvi@gmail.com>
2024-04-24 18:31:36 +0600

    Leap Year
    24 April 2024

---
+++ 6. SwitchStatement-LeapYear/src/pkg6/switchstatement/SwitchStatement.java
@@ -0,0 +1,42 @@
+package pkg6.switchstatement;
+
+import java.util.Scanner;
+
+public class SwitchStatement {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+//
+//
+//
+        //Leap Year:::
+//        System.out.println("Enter a year: ");
+//        int input = scanner.nextInt();
+//
+//        if ((input % 4 == 0 && input % 100 != 0) || input % 400 == 0) {
+//            System.out.println(input + " is a Leap Year.");
+//        } else {
+//            System.out.println(input + " is not a Leap Year.");
+//        }
+//
+//
+//
+//  // // // Leap Year :::
+//        System.out.println("Enter a year: ");
+//        int input = scanner.nextInt();
+//
+//        if (input % 4 == 0 || input % 400 == 0) {
+//            System.out.println(input + " is a Leap Year.");
+//        } else {
+//            System.out.println(input + " is not a Leap Year.");
+//        }
+//
+//
+//
+
+        
+    }
+
+}
