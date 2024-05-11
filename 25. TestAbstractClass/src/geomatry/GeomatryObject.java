c56e73c4bb5fb0e823d81a23b44208c022c5340b
mostofa-rezvi <mostofa.aminur.rezvi@gmail.com>
2024-05-11 18:50:55 +0600

    Abstract Class
    11 May 2024

---
+++ 25. TestAbstractClass/src/geomatry/GeomatryObject.java
@@ -0,0 +1,11 @@
+package geomatry;
+
+public abstract class GeomatryObject {
+
+    public GeomatryObject() {
+    }
+
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
+}
