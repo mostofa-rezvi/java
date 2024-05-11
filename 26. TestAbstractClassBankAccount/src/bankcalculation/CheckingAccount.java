09f5707a33cb03f233600fdcc7fa9f283f85d167
mostofa-rezvi <mostofa.aminur.rezvi@gmail.com>
2024-05-11 18:51:54 +0600

    Abstract Class (Bank)
    11 May 2024

---
+++ 26. TestAbstractClassBankAccount/src/bankcalculation/CheckingAccount.java
@@ -0,0 +1,46 @@
+package bankcalculation;
+
+public class CheckingAccount extends BankAccount {
+
+    private int withdrawalLimit;
+    private double withdrawalFee;
+
+    public CheckingAccount() {
+    }
+
+    public CheckingAccount(int withdrawalLimit, double withdrawalFee) {
+        this.withdrawalLimit = withdrawalLimit;
+        this.withdrawalFee = withdrawalFee;
+    }
+
+    public CheckingAccount(int withdrawalLimit, double withdrawalFee, double balance) {
+        super(balance);
+        this.withdrawalLimit = withdrawalLimit;
+        this.withdrawalFee = withdrawalFee;
+    }
+
+    @Override
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            if (withdrawalLimit > 0) {
+                balance -= amount;
+                withdrawalLimit--;
+            } else {
+                balance -= (amount + withdrawalFee);
+            }
+        } else {
+            System.out.println("Insufficient funds.");
+        }
+    }
+
+    @Override
+    public double calculateInterest() {
+        return 0;
+    }
+
+}
---
+++ 26. TestAbstractClassBankAccount/src/bankcalculation/SavingsAccount.java
@@ -0,0 +1,39 @@
+package bankcalculation;
+
+public class SavingsAccount extends BankAccount {
+
+    private double interestRate;
+
+    public SavingsAccount() {
+    }
+
+    public SavingsAccount(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public SavingsAccount(double interestRate, double balance) {
+        super(balance);
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient funds.");
+        }
+    }
+
+    @Override
+    public double calculateInterest() {
+        double interest = balance * interestRate;
+        return interest;
+    }
+
+}
