351a7ef326be2ef7f43bfc6dd66dc06d7ef5a44d
mostofa-rezvi <mostofa.aminur.rezvi@gmail.com>
2025-02-17 22:49:29 +0600

    S I

---
+++ JavaTask/src/javatask/Area.java
@@ -0,0 +1,14 @@
+
+package javatask;
+
+import java.util.Scanner;
+
+public class Area {
+    
+    Scanner input = new Scanner(System.in);
+    
+    int radius = 5;
+    double area = radius * radius * 3.1416;
+    
+    
+}
---
+++ JavaTask/src/javatask/JavaTask.java
@@ -0,0 +1,21 @@
+package javatask;
+
+public class JavaTask {
+
+    public static void main(String[] args) {
+        String name = "Rezvi";
+        System.out.println(name);
+
+        String a = "Rezvi";
+        System.out.println(a);
+
+        String s = "Rezvi";
+        System.out.println(s);
+
+        String sd = "Rezvi";
+        System.out.println(sd);
+
+        String as = "Rezvi";
+        System.out.println(as);
+    }
+}
